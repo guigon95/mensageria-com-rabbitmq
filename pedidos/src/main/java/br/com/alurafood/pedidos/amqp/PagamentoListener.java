1308cc06f9db6ce4c26fc8b48e5600dfd21e857f
Guilherme Gonçalves <guilherme_gon@live.com>
2022-11-15 17:05:37 -0300

    Incluindo produtores e consumidores

---
+++ pedidos/src/main/java/br/com/alurafood/pedidos/amqp/PagamentoListener.java
@@ -0,0 +1,19 @@
+package br.com.alurafood.pedidos.amqp;
+
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+import br.com.alurafood.pedidos.dto.PagamentoDto;
+
+@Component
+public class PagamentoListener {
+
+	@RabbitListener(queues = "pagamento.concluido")
+	public void recebeMensagem(PagamentoDto pagamento) {
+
+		System.out.println("Recebi a mensagem: " + pagamento.getId());
+		System.out.println("Número do pedido:"+ pagamento.getNumero());
+		System.out.println("Valor R$:" + pagamento.getValor());
+		System.out.println("Status:" + pagamento.getStatus());
+	}
+}
